610388c53e5c3c94aa62eaef2a7e2ea48df77f72
Lena Esipko <jessipko@gmail.com>
2019-01-17 19:16:04 +0200

    2 added

--- src/com/lecture/eighteen/HomeWork.java
+++ src/com/lecture/eighteen/HomeWork.java
@@ -18,5 +18,22 @@ public class HomeWork {
     public HomeWork(String name, int age) {
         this.name = name;
         this.age = age;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- src/com/lecture/eighteen/HomeWorkMain.java
+++ src/com/lecture/eighteen/HomeWorkMain.java
@@ -1,5 +1,7 @@
 package com.lecture.eighteen;
 
+import com.lecture.eight.Person;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -7,6 +9,7 @@ import java.util.List;
 
 public class HomeWorkMain {
     public static void main(String[] args) {
+        //I am adding 5 people to the
         HomeWork person1 = new HomeWork("Lena", 24);
         HomeWork person2 = new HomeWork("Murat", 24);
         HomeWork person3 = new HomeWork("Dmitri", 25);
@@ -23,7 +26,13 @@ public class HomeWorkMain {
         Iterator itr = persons.iterator();
         while (itr.hasNext()) {
             HomeWork pers = (HomeWork) itr.next();
-            System.out.println(pers.name + " " + pers.age );
+            System.out.println(pers.name + " " + pers.age);
+
+        }
+        System.out.println("");
+
+        for (HomeWork someone : persons) {
+            System.out.println(someone.getName() + " " + someone.getAge());
         }
 
     }
